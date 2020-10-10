d9eb2b4a5967173e77379efc39a3caac60afaa37
Sasha <justanotherreality@hotmail.com>
2020-10-10 21:48:53 +0200

    Slick on mobile

--- js/scripts.js
+++ js/scripts.js
@@ -56,6 +56,34 @@ $(function() {
     centerPadding: '0',
     prevArrow: '<span class="prev_arrow"><i class="fas fa-angle-left"></i></span>',
     nextArrow: '<span class="next_arrow"><i class="fas fa-angle-right"></i></span>',
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
   });
 });
 

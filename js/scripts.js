19441b998ac16097ee19ce6f5455a04a42d6cf2e
Sasha <justanotherreality@hotmail.com>
2020-11-09 22:39:38 +0100

    Fix 9.9.9

--- js/scripts.js
+++ js/scripts.js
@@ -86,6 +86,13 @@
     });
   });
 
+  // Counter
+  $(document).ready(function() {
+    $('.counter').counterUp({
+      delay: 10,
+      time: 1200
+    });
+  });
   // Animation
   const animItems = document.querySelectorAll('._anim-items');
 

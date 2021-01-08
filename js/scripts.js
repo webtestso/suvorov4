c761fb6a50173e89d0f26f68d2e2f614b4928540
Sasha <justanotherreality@hotmail.com>
2021-01-08 01:48:56 +0100

    4

--- js/scripts.js
+++ js/scripts.js
@@ -362,35 +362,35 @@
     });
   });
 
-  // Local storage
-
-  function saveLocalLang(language) {
-    let langs
-    if (localStorage.getItem(key:'langs') === null) {
-      langs = []
-    } else {
-      langs = JSON.parse(localStorage.getItem(key:'langs'))
-    }
-    langs.push(language)
-    localStorage.setItem('langs', JSON.stringify(langs))
-  }
-
-  function getLocalLang() {
-    let langs
-    if (localStorage.getItem(key:'langs') === null) {
-      langs = []
-    } else {
-      langs = JSON.parse(localStorage.getItem(key:'langs'))
-    }
-    langs.forEach(function (language) {
-      let lang = langs[langs.length - 1]
-      setTimeout( handler: () => {
-      $('.lang').each(function(index, item) {
-        $(this).text(arrLang[lang][$(this).attr('key')]);
-      });
-    }, timeout: 0)
-  })
-}  
+//   // Local storage
+//
+//   function saveLocalLang(language) {
+//     let langs
+//     if (localStorage.getItem(key:'langs') === null) {
+//       langs = []
+//     } else {
+//       langs = JSON.parse(localStorage.getItem(key:'langs'))
+//     }
+//     langs.push(language)
+//     localStorage.setItem('langs', JSON.stringify(langs))
+//   }
+//
+//   function getLocalLang() {
+//     let langs
+//     if (localStorage.getItem(key:'langs') === null) {
+//       langs = []
+//     } else {
+//       langs = JSON.parse(localStorage.getItem(key:'langs'))
+//     }
+//     langs.forEach(function (language) {
+//       let lang = langs[langs.length - 1]
+//       setTimeout( handler: () => {
+//       $('.lang').each(function(index, item) {
+//         $(this).text(arrLang[lang][$(this).attr('key')]);
+//       });
+//     }, timeout: 0)
+//   })
+// }
   // Collapse Navbar
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {

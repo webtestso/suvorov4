69acffdd59230af697a0b7e7aa15642653c7cdbc
Sasha <justanotherreality@hotmail.com>
2021-01-09 00:37:28 +0100

    One more

--- js/scripts.js
+++ js/scripts.js
@@ -147,7 +147,7 @@
 
   // Languages
 
-  // document.addEventListener('DOMContentLoaded', getLocalLang)
+  document.addEventListener('DOMContentLoaded', getLocalLang)
 
   var arrLang = {
     'en': {
@@ -354,42 +354,41 @@
   $(function() {
     $('.translate').click(function() {
       var lang = $(this).attr('id');
-
+      saveLocalLang(lang)
       $('.lang').each(function(index, item) {
         $(this).text(arrLang[lang][$(this).attr('key')]);
       });
     });
   });
 
-//   // Local storage
-//
-//   function saveLocalLang(language) {
-//     let langs
-//     if (localStorage.getItem(key:'langs') === null) {
-//       langs = []
-//     } else {
-//       langs = JSON.parse(localStorage.getItem(key:'langs'))
-//     }
-//     langs.push(language)
-//     localStorage.setItem('langs', JSON.stringify(langs))
-//   }
-//
-//   function getLocalLang() {
-//     let langs
-//     if (localStorage.getItem(key:'langs') === null) {
-//       langs = []
-//     } else {
-//       langs = JSON.parse(localStorage.getItem(key:'langs'))
-//     }
-//     langs.forEach(function (language) {
-//       let lang = langs[langs.length - 1]
-//       setTimeout( handler: () => {
-//       $('.lang').each(function(index, item) {
-//         $(this).text(arrLang[lang][$(this).attr('key')]);
-//       });
-//     }, timeout: 0)
-//   })
-// }
+  // Local storage
+
+  function saveLocalLang(language) {
+    let langs
+    if (localStorage.getItem('langs') === null) {
+      langs = []
+    } else {
+      langs = JSON.parse(localStorage.getItem('langs'))
+    }
+    langs.push(language)
+    localStorage.setItem('langs', JSON.stringify(langs))
+  }
+
+  function getLocalLang() {
+    let langs
+    if (localStorage.getItem('langs') === null) {
+      langs = []
+    } else {
+      langs = JSON.parse(localStorage.getItem('langs'))
+    }
+    langs.forEach(function (language) {
+      let lang = langs[langs.length - 1]
+      setTimeout( handler: () => {
+      $('.lang').each(function(index, item) {
+        $(this).text(arrLang[lang][$(this).attr('key')]);
+      });
+  })
+}
   // Collapse Navbar
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {

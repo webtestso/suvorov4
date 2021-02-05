4313a2d2217482112ae2565487273af3705474eb
Sasha <justanotherreality@hotmail.com>
2021-02-05 22:13:00 +0100

    Preloader

--- js/scripts.js
+++ js/scripts.js
@@ -146,7 +146,6 @@
   }
 
   // Languages
-
   document.addEventListener('DOMContentLoaded', getLocalLang)
 
   var arrLang = {
@@ -383,7 +382,6 @@ $('#name2').attr('placeholder','Your name *');
 $('#email2').attr('placeholder','Email *');
 
   // Local storage
-
   function saveLocalLang(language) {
     let langs
     if (localStorage.getItem('langs') === null) {
@@ -408,6 +406,37 @@ $('#email2').attr('placeholder','Email *');
         $(this).text(arrLang[lang][$(this).attr('key')]);
       });
   })
+}
+
+// Preloader
+var
+images = document.images,
+images_total_count = images.length,
+images_loaded_count = 0,
+preloader = document.getElementById('page_preloader'),
+perc_display = document.getElementById('load_perc');
+
+for( var i = 0; i < images_total_count; i++ )
+{
+  image_clone = new Image();
+  image_clone.onload = image_loaded;
+  image_clone.onerror = image_loaded;
+  image_clone.src = images[i].src;
+}
+
+function image_loaded() {
+  images_loaded_count++;
+  perc.display.innerHTML = (( (100 / images_total_count) * images_loaded_count ) << 0) + '%';
+
+  if( images_loaded_count >= images_total_count )
+  {
+    setTimeout(function(){
+      if( !preloader.classList.contains('done') )
+      {
+        preloader.classList.add('done');
+      }
+    }, 1000);
+  }
 }
   // Collapse Navbar
   var navbarCollapse = function() {

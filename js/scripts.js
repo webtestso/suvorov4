f2bf7c86fdc58e6e92369870073d5b23ab6a5441
Sasha <justanotherreality@hotmail.com>
2021-02-06 00:10:00 +0100

    Back

--- js/scripts.js
+++ js/scripts.js
@@ -408,36 +408,6 @@ $('#email2').attr('placeholder','Email *');
   })
 }
 
-// Preloader
-var
-images = document.images,
-images_total_count = images.length,
-images_loaded_count = 0,
-preloader = document.getElementById('page_preloader'),
-perc_display = document.getElementById('load_perc');
-
-for( var i = 0; i < images_total_count; i++ )
-{
-  image_clone = new Image();
-  image_clone.onload = image_loaded;
-  image_clone.onerror = image_loaded;
-  image_clone.src = images[i].src;
-}
-
-function image_loaded() {
-  images_loaded_count++;
-  perc.display.innerHTML = (( (100 / images_total_count) * images_loaded_count ) << 0) + '%';
-
-  if( images_loaded_count >= images_total_count )
-  {
-    setTimeout(function(){
-      if( !preloader.classList.contains('done') )
-      {
-        preloader.classList.add('done');
-      }
-    }, 1000);
-  }
-}
   // Collapse Navbar
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {

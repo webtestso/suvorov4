9006c0b22d057fa4d0f4823ffe84e6bf78c8e5d4
Sasha <justanotherreality@hotmail.com>
2021-02-19 16:27:33 +0100

    Try

--- js/scripts.js
+++ js/scripts.js
@@ -410,10 +410,12 @@ $('#email2').attr('placeholder','Email *');
 
 // Preloader
 $(document).ready(function() {
-  setTimeout(function() {
-    $('.wrapper').addClass('loaded');
 
-  }, 3000);
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color','#222222');
+    }, 3000);
+
 });
 
   // Collapse Navbar

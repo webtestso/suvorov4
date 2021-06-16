50213547dbd36fdd8ae64a4d8b1f2732c70057ca
Sasha <justanotherreality@hotmail.com>
2021-06-16 18:47:27 +0200

    Test

--- js/scripts_copy.js
+++ js/scripts_copy.js
@@ -37,6 +37,66 @@
     offset: 74,
   });
 
+  // Slick Slider
+  $(function() {
+    $('.multiple-items').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: false,
+      centerMode: false,
+      centerPadding: '0',
+      prevArrow: '<span class="prev_arrow"><i class="fas fa-angle-left"></i></span>',
+      nextArrow: '<span class="next_arrow"><i class="fas fa-angle-right"></i></span>',
+      responsive: [{
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 577,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+  });
+
+  // Slick Single
+  $(document).ready(function(){
+  $('.single-item').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    speed: 500,
+    cssEase: 'linear',
+    fade: true,
+  });
+});
+
 // Swiper
 new Swiper('image-slider', {
   navigation: {

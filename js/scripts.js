5cd7388c19c7a8597ead8915e9ff11edd4adddbe
Sasha <justanotherreality@hotmail.com>
2021-07-27 14:30:23 +0200

    Continue

--- js/scripts.js
+++ js/scripts.js
@@ -208,6 +208,39 @@ $('.btn-tab ul li button').on('click', function(){
   $('#'+dataid).addClass('current');
 
 });
+
+// Winemakers
+CSSPlugin.defaultTransformPerspective = 1000;
+
+//we set the backface
+TweenMax.set($(".cardBack"), {rotationY:-180});
+
+$.each($(".cardCont"), function(i,element) {
+
+	var frontCard = $(this).children(".cardFront"),
+      backCard = $(this).children(".cardBack"),
+      tl = new TimelineMax({paused:true});
+
+	tl
+		.to(frontCard, 1, {rotationY:180})
+		.to(backCard, 1, {rotationY:0},0)
+		.to(element, .5, {z:50},0)
+		.to(element, .5, {z:0},.5);
+
+	element.animation = tl;
+
+});
+
+$(".cardCont").hover(elOver, elOut);
+
+function elOver() {
+    this.animation.play();
+}
+
+function elOut() {
+    this.animation.reverse();
+}
+
   // Languages
   document.addEventListener('DOMContentLoaded', getLocalLang)
 

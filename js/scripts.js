d593c63b499fbafc1b02fdb5ab80ad88c7f24153
webtestso <justanotherreality@hotmail.com>
2021-08-24 16:28:05 +0200

    A lot of small changes before 1 last Update 2

--- js/scripts.js
+++ js/scripts.js
@@ -173,28 +173,6 @@ $('.btn-tab ul li button').on('click', function(){
 
 });
 
-// Counter
-
-const counterUp = window.counterUp.default
-
-const callback = entries => {
-	entries.forEach( entry => {
-		const el = entry.target
-		if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
-			counterUp( el, {
-				duration: 2000,
-				delay: 16,
-			} )
-			el.classList.add( 'is-visible' )
-		}
-	} )
-}
-
-const IO = new IntersectionObserver( callback, { threshold: 1 } )
-
-const el = document.querySelector( '.counter' )
-IO.observe( el )
-
   // Languages
   document.addEventListener('DOMContentLoaded', getLocalLang)
 

348dcbeacc068b2cf5c70d749e63e04186d48943
webtestso <justanotherreality@hotmail.com>
2022-01-06 13:55:55 +0100

    Counter Update

--- js/scripts.js
+++ js/scripts.js
@@ -40,6 +40,20 @@
         offset: 74,
     });
 
+    // Counter
+
+    $('.counter').each(function() {
+      $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+      }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function(now) {
+          $(this).text(Math.ceil(now));
+        }
+      });
+    });
+
     // Form
 
     var contactForm = function() {

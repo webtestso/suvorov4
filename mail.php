47d6696f198a2a65e8961bf5f556a31b85fed03e
webtestso <justanotherreality@hotmail.com>
2022-01-14 13:05:18 +0100

    Contact 4

--- mail.php
+++ mail.php
@@ -1,34 +1,36 @@
 <?php
+if(isset( $_POST['name']))
 $name = $_POST['name'];
+if(isset( $_POST['email']))
 $email = $_POST['email'];
+if(isset( $_POST['message']))
 $message = $_POST['message'];
+if(isset( $_POST['subject']))
 $subject = $_POST['subject'];
-header('Content-Type: application/json');
 if ($name === ''){
-print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
-exit();
+echo "Name cannot be empty.";
+die();
 }
 if ($email === ''){
-print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
-exit();
+echo "Email cannot be empty.";
+die();
 } else {
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
-exit();
+echo "Email format invalid.";
+die();
 }
 }
 if ($subject === ''){
-print json_encode(array('message' => 'Subject cannot be empty', 'code' => 0));
-exit();
+echo "Subject cannot be empty.";
+die();
 }
 if ($message === ''){
-print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
-exit();
+echo "Message cannot be empty.";
+die();
 }
 $content="From: $name \nEmail: $email \nMessage: $message";
-$recipient = "alexandr.neruh@suvorov-vin.md";
+$recipient = "justanotherreality@hotmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
-print json_encode(array('message' => 'Email successfully sent!', 'code' => 1));
-exit();
+echo "Email sent!";
 ?>

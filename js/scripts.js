62f8125092866050bdf3a0c93acde4fca3c6fa8b
webtestso <justanotherreality@hotmail.com>
2022-01-14 12:42:27 +0100

    Contact 3

--- js/scripts.js
+++ js/scripts.js
@@ -56,36 +56,31 @@
 
     // Form
 
-    const form = document.querySelector("form"),
-    statusTxt = form.querySelector(".button-area span");
+    document.getElementById('status').innerHTML = "Sending...";
+    formData = {
+    'name'     : $('input[name=name]').val(),
+    'email'    : $('input[name=email]').val(),
+    'subject'  : $('input[name=subject]').val(),
+    'message'  : $('textarea[name=message]').val()
+    };
 
-    form.onsubmit = (e)=>{
-      e.preventDefault();
-      statusTxt.style.color = "#0D6EFD";
-      statusTxt.style.display = "block";
-      statusTxt.innerText = "Sending your message...";
-      form.classList.add("disabled");
 
-      let xhr = new XMLHttpRequest();
-      xhr.open("POST", "message.php", true);
-      xhr.onload = ()=>{
-        if(xhr.readyState == 4 && xhr.status == 200){
-          let response = xhr.response;
-          if(response.indexOf("Email and message field is required!") != -1 || response.indexOf("Enter a valid email address!") != -1 || response.indexOf("Sorry, failed to send your message!") != -1){
-            statusTxt.style.color = "red";
-          }else{
-            form.reset();
-            setTimeout(()=>{
-              statusTxt.style.display = "none";
-            }, 3000);
-          }
-          statusTxt.innerText = response;
-          form.classList.remove("disabled");
-        }
-      }
-      let formData = new FormData(form);
-      xhr.send(formData);
+    $.ajax({
+    url : "mail.php",
+    type: "POST",
+    data : formData,
+    success: function(data, textStatus, jqXHR)
+    {
+
+    $('#status').text(data.message);
+    if (data.code) //If mail was sent successfully, reset the form.
+    $('#contact-form').closest('form').find("input[type=text], textarea").val("");
+    },
+    error: function (jqXHR, textStatus, errorThrown)
+    {
+    $('#status').text(jqXHR);
     }
+    });
 
     // Swiper
 

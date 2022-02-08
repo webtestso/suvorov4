438aea18e1fb327dfe24ae1f0497e6d52166ffa3
webtestso <justanotherreality@hotmail.com>
2022-02-08 16:04:24 +0100

    Contact Test 3.8

---
+++ constant.php
@@ -0,0 +1,5 @@
+<?php
+//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
+define('SITE_KEY',"6LdcoGYeAAAAAGTTFWKcvw5LwkpHLSUkHzti-UoZ");
+define('SECRET_KEY',"6LdcoGYeAAAAAAJ7HR1pY5cU_7JnkZEb46IbsTTS")
+?>
--- index.php
+++ index.php
@@ -66,7 +66,40 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/classie/1.0.1/classie.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
     <script src="https://www.google.com/recaptcha/api.js"></script>
-    <script src="ajax_contact.js"></script>
+    <script>
+    $(document).ready(function (e){
+      $("#frmContact").on('submit',(function(e){
+        e.preventDefault();
+        $("#mail-status").hide();
+        $('#send-message').hide();
+        $('#loader-icon').show();
+        $.ajax({
+          url: "contact.php",
+          type: "POST",
+          dataType:'json',
+          data: {
+          "name":$('input[name="name"]').val(),
+          "email":$('input[name="email"]').val(),
+          "phone":$('input[name="phone"]').val(),
+          "content":$('textarea[name="content"]').val(),
+          "g-recaptcha-response":$('textarea[id="g-recaptcha-response"]').val()},
+          success: function(response){
+          $("#mail-status").show();
+          $('#loader-icon').hide();
+          if(response.type == "error") {
+            $('#send-message').show();
+            $("#mail-status").attr("class","error");
+          } else if(response.type == "message"){
+            $('#send-message').hide();
+            $("#mail-status").attr("class","success");
+          }
+          $("#mail-status").html(response.text);
+          },
+          error: function(){}
+        });
+      }));
+    });
+    </script>
     <!-- Preloader -->
     <script src="js/queryloader2.min.js" type="text/javascript"></script>
     <script type="text/javascript">
@@ -423,45 +456,70 @@
         </div>
       </div>
           </div>
-          <div class="container mt-5">
-
-            <h2>Implement Google reCAPTCHA in PHP Contact Form</h2>
-            <?php include('scripts/form.php'); ?>
-            <!-- Error messages -->
-            <?php if(!empty($response)) {?>
-            <div class="form-group col-12 text-center">
-              <div class="alert text-center <?php echo $response['status']; ?>">
-                <?php echo $response['message']; ?>
-              </div>
-            </div>
-            <?php }?>
-            <!-- Contact form -->
-            <form action="" name="contactForm" id="contactForm" method="post" enctype="multipart/form-data" novalidate>
-              <div class="form-group">
-                <label>Name</label>
-                <input type="text" class="form-control" name="name" id="name">
-              </div>
-              <div class="form-group">
-                <label>Email</label>
-                <input type="email" class="form-control" name="email" id="email">
-              </div>
-              <div class="form-group">
-                <label>Subject</label>
-                <input type="text" class="form-control" name="subject" id="subject">
-              </div>
-              <div class="form-group">
-                <label>Message</label>
-                <textarea class="form-control" rows="4" name="message" id="message"></textarea>
-              </div>
-              <div class="form-group">
-                <!-- Google reCAPTCHA block -->
-                <div class="g-recaptcha" data-sitekey="6LdcoGYeAAAAAGTTFWKcvw5LwkpHLSUkHzti-UoZ"></div>
-              </div>
-              <div class="form-group">
-                <input type="submit" name="send" value="Send" class="btn btn-dark btn-block">
-              </div>
-            </form>
-          </div>
+          <style>
+          .label {margin: 2px 0;}
+          .field {margin: 0 0 20px 0;}
+            .content {width: 960px;margin: 0 auto;}
+            h1, h2 {font-family:"Georgia", Times, serif;font-weight: normal;}
+            div#central {margin: 40px 0px 100px 0px;}
+            @media all and (min-width: 768px) and (max-width: 979px) {.content {width: 750px;}}
+            @media all and (max-width: 767px) {
+              body {margin: 0 auto;word-wrap:break-word}
+              .content {width:auto;}
+              div#central {	margin: 40px 20px 100px 20px;}
+            }
+            body {font-family: 'Helvetica',Arial,sans-serif;background:#ffffff;margin: 0 auto;-webkit-font-smoothing: antialiased;  font-size: initial;line-height: 1.7em;}
+            input, textarea {width:100%;padding: 15px;font-size:1em;border: 1px solid #A1A1A1;	}
+            button {
+              padding: 12px 60px;
+              background: #5BC6FF;
+              border: none;
+              color: rgb(40, 40, 40);
+              font-size:1em;
+              font-family: "Georgia", Times, serif;
+              cursor: pointer;
+            }
+            #message {  padding: 0px 40px 0px 0px; }
+            #mail-status {
+              padding: 12px 20px;
+              width: 100%;
+              display:none;
+              font-size: 1em;
+              font-family: "Georgia", Times, serif;
+              color: rgb(40, 40, 40);
+            }
+            .error{background-color: #F7902D;  margin-bottom: 40px;}
+            .success{background-color: #48e0a4; }
+            .g-recaptcha {margin: 0 0 25px 0;}
+          </style>
+          <div class="content">
+        		<h1>Contact Form</h1>
+        		<p>Send your comments through this form and we will get back to you. </p>
+        		<div id="message">
+        		<form id="frmContact" action="" method="POST" novalidate="novalidate">
+        			<div class="label">Name:</div>
+        			<div class="field">
+        				<input type="text" id="name" name="name" placeholder="enter your name here" title="Please enter your name" class="required" aria-required="true" required>
+        			</div>
+        			<div class="label">Email:</div>
+        			<div class="field">
+        				<input type="text" id="email" name="email" placeholder="enter your email address here" title="Please enter your email address" class="required email" aria-required="true" required>
+        			</div>
+        			<div class="label">Phone Number:</div>
+        			<div class="field">
+        				<input type="text" id="phone" name="phone" placeholder="enter your phone number here" title="Please enter your phone number" class="required phone" aria-required="true" required>
+        			</div>
+        			<div class="label">Comments:</div>
+        			<div class="field">
+        				<textarea id="comment-content" name="content" placeholder="enter your comments here"></textarea>
+        			</div>
+        			<div class="g-recaptcha" data-sitekey="<?php echo SITE_KEY; ?>"></div>
+        			<div id="mail-status"></div>
+        			<button type="Submit" id="send-message" style="clear:both;">Send Message</button>
+        		</form>
+        		<div id="loader-icon" style="display:none;"><img src="assets/img/icons/refresh.png" /></div>
+        		</div>
+        	</div>
         </div>
       </figure>
     </section>
